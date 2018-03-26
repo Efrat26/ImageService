b7bba6df52f6cf8bde6df55e3aee49390519cd4e
efiso <efisofer26@gmail.com>
2018-03-26 21:03:02 +0300

    modified the add file implementation, still not perfect. need to check it on a real folder to understand what need to be changed.

--- ImageService/Modal/ImageServiceModal.cs
+++ ImageService/Modal/ImageServiceModal.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ImageService.Modal
 {
@@ -19,15 +22,82 @@ namespace ImageService.Modal
         }
         public string AddFile(string path, out bool result)
         {
-            result = true;
-            return null;
+            DateTime d = GetDateTakenFromImage(path);
+            int year = this.GetYearAsNumber(d);
+            int month = this.GetMonthAsNumber(d);
+            string newPath = this.m_OutputFolder + '\\' + year;
+            System.IO.Directory.CreateDirectory(newPath);//create only if not exist
+            newPath = newPath + '\\' + month;
+            System.IO.Directory.CreateDirectory(newPath);
+            if (this.CreateFile(newPath))
+            {
+                //create also in thumbnail
+                this.CreateThumbnailCopy(path);
+                result = true;
+                return "success";
+            }
+            result = false;
+            return "error";
         }
         public string MoveFile(string path, out bool result)
         {
             result = true;
             return null;
         }
-
+        //create a copy in thumbnail
+        private bool CreateThumbnailCopy(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                fs.SetLength(this.m_thumbnailSize);
+                return true;
+            }
+        }
+        //create file int the folder
+        private bool CreateFile(string path)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(path))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        //get the date image was created
+        private static DateTime GetDateTakenFromImage(string path)
+        {
+            return File.GetCreationTime(path);
+        }
+        //return month as number
+        private int GetMonthAsNumber(DateTime d)
+        {
+            string sMonth = DateTime.Now.ToString("MM");
+            return Int32.Parse(sMonth);
+        }
+        //return month as number
+        private int GetYearAsNumber(DateTime d)
+        {
+            string sYear = DateTime.Now.ToString("YY");
+            return Int32.Parse(sYear);
+        }
+        //check if directory exist
+        private bool CheckIfDirExist(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         #endregion
     }
 }
